4745d63eeb0b68d8c95077eaf3192841b10c902b
Alex Lozano <19170025@uttcampus.edu.mx>
2020-11-25 12:37:00 -0600

    primer commint

---
+++ app/src/main/java/com/example/loteria/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.loteria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void verEstadisticas(View view) {
+    }
+
+    public void sumerNumero(View view) {
+    }
+
+    public void enviarNumero(View view) {
+    }
+}
